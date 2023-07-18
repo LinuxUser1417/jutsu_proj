35684e4d7f598e27e573d364589a7aef273ed90d
Iskender <iskenderhot@gmail.com>
2023-07-19 04:14:03 +0600

    dadad

--- main/settings.py
+++ main/settings.py
@@ -182,18 +182,7 @@ SIMPLE_JWT = {
     "TOKEN_BLACKLIST_SERIALIZER": "rest_framework_simplejwt.serializers.TokenBlacklistSerializer",
 }
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
-    }
-}
 
-SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
-SESSION_CACHE_ALIAS = 'default'
 
 # REDIS_HOST = 'redis'
 REDIS_HOST = config('REDIS_HOST')
